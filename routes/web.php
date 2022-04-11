4c1804e6c1d81e520eae4f92f14895d6e0d3d436
codeashish34 <ankit1x01@gmail.com>
2022-04-11 18:43:46 +0530

    upd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [AdminViewController::class, 'index'])->name('home');
